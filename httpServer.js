460b2116a822784480705246ed4ceab8b520427e
aakarsheeee <aakarshee.rohatgi@gmail.com>
2022-08-02 21:41:02 +0530

    last commit

--- httpServer.js
+++ httpServer.js
@@ -2,10 +2,5 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-<<<<<<< HEAD
   res.end('Hello World from aakarshee');
 }).listen(3020);
-=======
-  res.end('Hello World!');
-}).listen(3020);
->>>>>>> 4c5b1b84b592133d252a0c441fad13e28952c670
